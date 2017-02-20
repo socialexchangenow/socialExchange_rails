ca8620eca428063e47e92fba61026b816fe7c5e9
Wesley Nelson <wesleynelson2012@gmail.com>
2017-02-19 22:15:16 -0500

    Added indidivual scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20170220030653) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "individuals", force: :cascade do |t|
+    t.string   "source",     limit: 3
+    t.string   "sourceID"
+    t.string   "firstName"
+    t.string   "lastName"
+    t.string   "email"
+    t.datetime "created_at",           null: false
+    t.datetime "updated_at",           null: false
+  end
 
 end
