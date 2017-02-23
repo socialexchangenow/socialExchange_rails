3252dd8c08fb9859d26bb3a2f94bc845893770c1
Wesley Nelson <wesleynelson2012@gmail.com>
2017-02-23 15:57:13 -0500

    debugging hook

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,7 +12,8 @@ Rails.application.configure do
 
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
-  config.action_controller.perform_caching = true
+  # TODO: Change this back to true when done debugging puts in prod
+  config.action_controller.perform_caching = false
 
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
