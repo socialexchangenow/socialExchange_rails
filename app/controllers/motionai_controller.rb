4ecf6697337271382cbfa80e6579d9d6372c0b77
Wesley Nelson <wesleynelson2012@gmail.com>
2017-02-23 15:48:07 -0500

    debugging hook

--- app/controllers/motionai_controller.rb
+++ app/controllers/motionai_controller.rb
@@ -1,7 +1,8 @@
 class MotionaiController < ApplicationController
+  skip_before_action :verify_authenticity_token
 
   def globalhook
-    #puts "motion.ai: GlobalHook: params=#{params.inspect}"
+    puts "motion.ai: GlobalHook: params=#{params.inspect}"
     head :ok
   end
 
